6c14a158d57f5371485bff46e7ad811f998c2acc
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-02-04 17:20:31 -0500

    updated

--- Exercise3_5.cpp
+++ Exercise3_5.cpp
@@ -1,3 +1,10 @@
+/*
+Author: Evan Lu
+Course: CSCI-135
+Instructor: Professor Maryash
+Assignment:  Write a program that reads an integer and prints whether it is negative, zero, or positive.
+*/
+
 #include <iostream>
 
 int main(){
