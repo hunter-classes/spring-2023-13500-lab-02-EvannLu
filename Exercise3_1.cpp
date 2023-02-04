96a5bab74a68592c0eb1d868f6b2ecfd74688a36
Evan Lu <evan.l60@myhunter.cuny.edu>
2023-02-04 16:56:51 -0500

    updated

---
+++ Exercise3_1.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Evan Lu
+Course: CSCI-135
+Instructor: Professor Zamansky
+Assignment:  Write a program that reads an integer and 
+             prints whether it is negative, zero, or positive.
+*/
+
+#include <iostream>
+
+int main(){ 
+    int input; 
+
+    std::cout << "Enter a  integer: ";
+    std::cin >> input; 
+
+    if(input == 0){
+        std::cout << input << " is zero"; 
+    } else if(input > 0){
+        std::cout << input << " is positive";
+    } else {
+        std::cout << input << " is negative";
+    }
+
+    return 0; 
+}
